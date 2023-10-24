db3859aff4e6621ba368ae5ac592628495aa5399
volpifabio <volpifabio.fvp@gmail.com>
2023-10-23 22:01:02 -0300

    array e repetição

---
+++ src/ExemploFor.java
@@ -0,0 +1,7 @@
+public class ExemploFor{
+    public static void main(String[] args) {
+        for( int carneiros = 1; carneiros <=20; carneiros ++){
+            System.out.println("Contando carneiros " + carneiros);
+        }
+    }
+}
